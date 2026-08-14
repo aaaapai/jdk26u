55d1ee79ba35a994ff1750eec184009cad1a2a09
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-08-14 23:55:26 +0800

    Update ProcessImpl.java

--- src/java.base/unix/classes/java/lang/ProcessImpl.java
+++ src/java.base/unix/classes/java/lang/ProcessImpl.java
@@ -100,6 +100,7 @@ final class ProcessImpl extends Process {
             // Should be value of a LaunchMechanism enum
             LaunchMechanism lm = LaunchMechanism.valueOf(s.toUpperCase(Locale.ROOT));
             switch (OperatingSystem.current()) {
+                case ANDROID:
                 case LINUX: {
                     // All options are valid for Linux, but VFORK is deprecated and results
                     // in a warning
@@ -114,6 +115,7 @@ final class ProcessImpl extends Process {
                     }
                     return lm;
                 }
+                case IOS:
                 case AIX:
                 case MACOS:
                     if (lm != LaunchMechanism.VFORK) {
@@ -332,6 +334,7 @@ final class ProcessImpl extends Process {
      */
     void initStreams(int[] fds, boolean forceNullOutputStream) throws IOException {
         switch (OperatingSystem.current()) {
+            case ANDROID:
             case LINUX:
             case MACOS:
                 stdin = (fds[0] == -1) ?
@@ -464,6 +467,7 @@ final class ProcessImpl extends Process {
 
     private void destroy(boolean force) {
         switch (OperatingSystem.current()) {
+            case ANDROID:
             case LINUX:
             case MACOS:
             case AIX:
